cd40dc18ee71f136b802e825f26860857d9511df
Danny Robinson <dannyrobinson@gmx.com>
2018-09-23 00:27:06 +0100

    js-to-jl

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -2,12 +2,15 @@ function convert (text) {
   const regex = [
     [/('.*?)"(.*?)"(.*?')/g, '$1\\"$2\\"$3'],
     // escape any " in string literals
-    [/for\s?\(\s?(?:var|let)\s(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\)\s?([^{]+$)/gm, 'for $1 in $2:$3\n$4end\n'],
-    [/for\s?\(\s?(?:var|let)\s(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\s?\)\s?{?/gm, 'for $1 in $2:$3'],
+    [/([a-zA-Z]*)\.length/, 'length($1)'],
+    // length from property to function
+    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\)\s?([^{]+$)/gm, 'for $1 in $2:$3\n$4end\n'],
+    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\s?\)\s?{?/gm, 'for $1 in $2:$3'],
+    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+=\s*(\d+)\s*\)\s*{?/gm, 'for $1 in $2:$4:$3'],
     // for loops
     [/(?:Test\.)?describe\(["'`](.+?)["'`],.*{/g, 'facts("$1") do'],
     // Test.describe("description") to facts("description") do
-    [/(?:Test\.)?it\(["'`](.+?)["'`],.*{/g, '  context("$1") do'],
+    [/(?:Test\.)?it\(["'`](.+?)["'`](.+?),.*{/g, '  context("$1") do'],
     // Test.it("description") to context("description") do
     [/Test\.expect\((.+?)\s?,\s?["'`](.+)["'`]\);?}?/g,
       (match, p1, p2) => {
@@ -34,15 +37,18 @@ function convert (text) {
     // } to end
     [/{\s*$/gm, ' do'],
     // { to do
+    [/function (.+?)\s?{\s?(?:return )([^}]+?)\s?;?\s?}/g, (m, p1, p2) => p1.replace(/\s+\(/, '(') + ' = ' + p2],
+    // one liner function declarations to short form
     [/(var|let)\s?/g, ''],
     // no more vars or lets
-    [/Math.random\(/g, 'rand('],
-    // random number
+    [/Math.random/g, 'rand'],
+    [/Math.floor/g, 'floor'],
+    // fix Math functions
     [/\s?=>\s?/g, ' -> '], // skinny arrow functions
-    [/;\s?$/gm, ''],  // strip line end
-    [/\${(.+?)}/g, '\$($1)'], // template literals
+    [/;?\s*$/gm, ''],  // strip line end semicolons and spaces
+    [/\${(.+?)}/g, '\$($1)'], // template literals interpolation fix
     [/\n\n/g, '\n'], // no double new lines
-    [/else if/, 'elseif']
+    [/else if/g, 'elseif']
   ]
   for (const r of regex)
     text = text.replace(r[0], r[1])
