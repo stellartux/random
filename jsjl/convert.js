c3cf79eda76fc751dbf60dc2c1a7dd5444de2bd5
Danny Robinson <dannyrobinson@gmx.com>
2018-09-22 23:08:00 +0100

    js-to-jl

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -9,18 +9,26 @@ function convert (text) {
     // Test.describe("description") to facts("description") do
     [/(?:Test\.)?it\(["'`](.+?)["'`],.*{/g, '  context("$1") do'],
     // Test.it("description") to context("description") do
-    [/Test\.expect\((.+?)\s?,\s?["'`](.+)["'`]\);?}?/g, '    @fact $1 --> true "$2"'],
+    [/Test\.expect\((.+?)\s?,\s?["'`](.+)["'`]\);?}?/g,
+      (match, p1, p2) => {
+      return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase()) + ' --> true "' + p2 + '"'
+    }],
     // Test.expect(x, y) to @fact x --> y
-    [],
-    [/Test\.assertEquals\((.+?\(.+?\))\s*?,\s*?(.+?),\s?["'](.+?)["']\)+;?/g, '    @fact $1 --> $2 "$3"'],
+    [/Test\.assertEquals\((.+?\(.+?\))\s*,\s*(.+?),\s?["'](.+?)["']\)+;?/g,
+      (match, p1, p2, p3) => {
+      return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase() + '(') + ' --> ' + p2 + ' "' + p3 + '"'
+    }],
     // Test.assertEquals(x, y, z) to @fact x --> y "z"
-    [/Test\.assertEquals\((.+?\(.+?\))\s*?,\s*?(.+?)\)+;?/g, '    @fact $1 --> $2'],
+    [/Test\.assertEquals\((.+?\(.+?\))\s*,\s*(.+?)\)+;?/g,
+      (match, p1, p2) => {
+      return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase() + '(') + ' --> ' + p2
+    }],
     // Test.assertEquals(x, y) to @fact x --> y
     [/===/, '=='],
     // threequals to twoquals
     [/}\);?/g, 'end\n'],
     // }); to end
-    [/'(.+?)[^\\]?'/g, '"$1"'],
+    [/'(.+?[^\\]?)'/g, '"$1"'],
     // '' string literals to "" string literals
     [/^(\s*)};?$/gm, '$1end'],
     // } to end
@@ -29,14 +37,12 @@ function convert (text) {
     [/(var|let)\s?/g, ''],
     // no more vars or lets
     [/Math.random\(/g, 'rand('],
-    // random numbers
-    [/\s?=>\s?/g, ' -> '],
-    // skinny arrow functions
-    [/;\s?$/gm, ''],
-    // strip line end
-    [/\${(.+?)}/g, '\$($1)'],
-    [/\n\n/g, '\n']
-  // no double new lines
+    // random number
+    [/\s?=>\s?/g, ' -> '], // skinny arrow functions
+    [/;\s?$/gm, ''],  // strip line end
+    [/\${(.+?)}/g, '\$($1)'], // template literals
+    [/\n\n/g, '\n'], // no double new lines
+    [/else if/, 'elseif']
   ]
   for (const r of regex)
     text = text.replace(r[0], r[1])
