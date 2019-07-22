cb1ee4f435ee8b221f8001776d147edaf0a82a56
Danny Robinson <dannyrobinson@gmx.com>
2019-07-22 23:49:14 +0100

    jsjl better newline

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -11,21 +11,21 @@ function convert (text) {
     [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\s?\)\s*{?/gm, 'for $1 in $2:$3'],
     [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+=\s*(\d+)\s*\)\s*{?/gm, 'for $1 in $2:$4:$3'],
     // Test.describe("description") to facts("description") do
-    [/^\s*(?:Test\.)?describe\(["'`](.+?)["'`],.*{/gmi, 'facts("$1") do'],
+    [/^\s*(?:Test\.)?describe\(["'`](.+?)["'`],.*\n?\s*{/gmi, 'facts("$1") do'],
     // Test.it("description") to context("description") do
-    [/^\s*(?:Test\.)?it\s*\(["'`](.+?)["'`].*?{/gmi, '  context("$1") do'],
+    [/^\s*(?:Test\.)?it\s*\(["'`](.+?)["'`].*?\n?\s*{/gmi, '  context("$1") do'],
     // Test.expect(x, y) to @fact x --> y
     [/^\s*Test\.expect\((.+?)\s*,\s*["'`](.+)["'`]\);?}?/gm,
       (match, p1, p2) => {
       return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '')) + ' --> true "' + p2 + '"'
     }],
     // Test.assertEquals(x, y, z) to @fact x --> y
-    [/^\s*(?:Test\.)?assert(?:(?:[dD]eep)?Equals?)?\s*\((.+?\(.*?\))\s*,\s*(.+?),\s*["'](.+?)["']\)+;?/gmi,
+    [/^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?)?\s*\((.+?\(.*?\))\s*,\s*(.+?),\s*["'](.+?)["']\)+;?/gmi,
       (match, p1, p2, p3) => {
       return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '') + '(') + ' --> ' + p2
     }],
     // Test.assertEquals(x, y) to @fact x --> y
-    [/^\s*(?:Test\.)?assert\.?(?:[dD]eep)?Equals?\((.+?\(.*?\))\s*,\s*(.+?)\);?/gmi,
+    [/^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\((.+?\(.*?\))\s*,\s*(.+?)\);?/gmi,
       (match, p1, p2) => {
       return '    @fact ' + p1.replace(/(.+)\(/g, (m, q1) => q1.toLowerCase().replace('_', '') + '(') + ' --> ' + p2
     }],
@@ -42,7 +42,7 @@ function convert (text) {
     // '' string literals to "" string literals
     [/'(.+?[^\\]?)??'/g, '"$1"'],
     // one liner function declarations to short form
-    [/function (.+?)\s?{\s?(?:return )([^}]+?)\s?;?\s?}/g, (m, p1, p2) => p1.replace(/\s+\(/, '(') + ' = ' + p2],
+    [/function (.+?)\s?\n?\s*{\n?\s*(?:return )([^}]+?)\s?;?\s?}/g, (m, p1, p2) => p1.replace(/\s+\(/, '(') + ' = ' + p2],
     // } to end
     [/^(\s*)};?$/gm, '$1end'],
     // { to do
@@ -69,7 +69,7 @@ function convert (text) {
     [/\n\n/g, '\n'],
     [/else if/g, 'elseif'],
     // remove brackets on if statements
-    [/if\s*\((.+)\) *{? */gm, 'if $1 '],
+    [/if\s*\((.+)\) *\n? *{? */gm, 'if $1 '],
     [/null/g, 'nothing']
   ]
   for (const r of regex)
