7b0f5ff128ad0b9f83df3d2365706cc346a083a7
Danny Robinson <dannyrobinson@gmx.com>
2020-01-13 23:50:58 +0000

    jsjl occursin fix

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -92,7 +92,7 @@ const polys = {
     backfill: 'import Base.replace\n  replace(a, p::Pair; count=typemax(Int)) = replace(a, p[1], p[2], count)'
   },
   'occursin': {
-    backfill: 'occursin = ismatch\n occursin(c::Char, s::String) = contains(s, string(c))\n  occursin(c::String, s::String) = contains(s, c)'
+    backfill: 'occursin(re::Regex, s::String) = ismatch\n occursin(c, s::String) = contains(s, string(c))
   },
   'isdefined': {
     description: '@isdefined',
