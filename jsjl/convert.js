1d690f48a1ac2289931dc85d4351862bb3af0f83
Danny Robinson <dannyrobinson@gmx.com>
2020-02-18 22:06:31 +0000

    jsjl: Nothing polyfill

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -1,4 +1,4 @@
-function convert (text) {
+function convert(text) {
   const regex = [
     // escape any " in string literals
     [/('.*?)"(.*?)"(.*?')/g, '$1\\"$2\\"$3'],
@@ -7,34 +7,86 @@ function convert (text) {
     // x.indexOf(y) to indexin(x, y)
     [/(\w+)\.indexOf\(([^)]+\))/g, 'indexin($1, $2)'],
     // for loops
-    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\)\s*([^{]+$)/gm, 'for $1 in $2:$3\n$4end\n'],
-    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\s?\)\s*{?/gm, 'for $1 in $2:$3'],
-    [/for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+=\s*(\d+)\s*\)\s*{?/gm, 'for $1 in $2:$4:$3'],
+    [
+      /for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\)\s*([^{]+$)/gm,
+      'for $1 in $2:$3\n$4end\n',
+    ],
+    [
+      /for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+\+\s?\)\s*{?/gm,
+      'for $1 in $2:$3',
+    ],
+    [
+      /for\s*\(\s*(?:var|let)\s*(.+?)=(.+?);(?:.+?)<(.+?);.+?\+=\s*(\d+)\s*\)\s*{?/gm,
+      'for $1 in $2:$4:$3',
+    ],
     // Test.describe("description") to facts("description") do
-    [/^\s*(?:Test\.)?describe\s*\(\s*["'`](.+?)["'`],.*\n?\s*{/gmi, 'facts("$1") do'],
+    [
+      /^\s*(?:Test\.)?describe\s*\(\s*["'`](.+?)["'`],.*\n?\s*{/gim,
+      'facts("$1") do',
+    ],
     // Test.it("description") to context("description") do
-    [/^\s*(?:Test\.)?it\s*\(\s*["'`](.+?)["'`].*?\n?\s*{/gmi, '  context("$1") do'],
+    [
+      /^\s*(?:Test\.)?it\s*\(\s*["'`](.+?)["'`].*?\n?\s*{/gim,
+      '  context("$1") do',
+    ],
     // Test.expect(x, y) to @fact x --> y
-    [/^\s*Test\.expect\((.+?)\s*,\s*["'`](.+)["'`]\);?}?/gm,
+    [
+      /^\s*Test\.expect\((.+?)\s*,\s*["'`](.+)["'`]\);?}?/gm,
       (match, p1, p2) => {
-      return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '')) + ' --> true "' + p2 + '"'
-    }],
+        return (
+          '    @fact ' +
+          p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '')) +
+          ' --> true "' +
+          p2 +
+          '"'
+        )
+      },
+    ],
     // Test.assertEquals(x, y, z) to @fact x --> y
-    [/^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\s*\((.+?\(.*?\))\s*,\s*(.+?),\s*["'](.+?)["']\)+;?/gmi,
+    [
+      /^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\s*\((.+?\(.*?\))\s*,\s*(.+?),\s*["'](.+?)["']\)+;?/gim,
       (match, p1, p2, p3) => {
-      return '    @fact ' + p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '') + '(') + ' --> ' + p2
-    }],
+        return (
+          '    @fact ' +
+          p1.replace(
+            /(.+)\(/,
+            (m, q1) => q1.toLowerCase().replace('_', '') + '('
+          ) +
+          ' --> ' +
+          p2
+        )
+      },
+    ],
     // Test.assertEquals(x, y) to @fact x --> y
-    [/^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\((.+?\(.*?\))\s*,\s*(.+?)\);?/gmi,
+    [
+      /^\s*(?:Test\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\((.+?\(.*?\))\s*,\s*(.+?)\);?/gim,
       (match, p1, p2) => {
-      return '    @fact ' + p1.replace(/(.+)\(/g, (m, q1) => q1.toLowerCase().replace('_', '') + '(') + ' --> ' + p2
-    }],
+        return (
+          '    @fact ' +
+          p1.replace(
+            /(.+)\(/g,
+            (m, q1) => q1.toLowerCase().replace('_', '') + '('
+          ) +
+          ' --> ' +
+          p2
+        )
+      },
+    ],
     // Test.assertSimilar(x, y) to @fact x --> roughly(y)
-    [/^\s*Test\.assertSimilar\((.+?(?:\(.+?\))?)\s*,\s*(.+?)\);?/gmi, '    @fact $1 --> roughly($2)'],
+    [
+      /^\s*Test\.assertSimilar\((.+?(?:\(.+?\))?)\s*,\s*(.+?)\);?/gim,
+      '    @fact $1 --> roughly($2)',
+    ],
     // assert.throw(x, y) to @fact_throws y x
-    [/^\s*(?:Test\.)?assert\.?[tT]hrows?\s*\( *(.+), *(.+) *\)/gm, '    @fact_throws $2 $1'],
+    [
+      /^\s*(?:Test\.)?assert\.?[tT]hrows?\s*\( *(.+), *(.+) *\)/gm,
+      '    @fact_throws $2 $1',
+    ],
     // assert.throw(x) to @fact_throws x
-    [/^\s*(?:Test\.)?assert\.?[tT]hrows?\s*\( *(.+) *\)/gm, '    @fact_throws $1'],
+    [
+      /^\s*(?:Test\.)?assert\.?[tT]hrows?\s*\( *(.+) *\)/gm,
+      '    @fact_throws $1',
+    ],
     // threequals to twoquals
     [/===/g, '=='],
     // }); to end
@@ -42,7 +94,10 @@ function convert (text) {
     // '' string literals to "" string literals
     [/'(.+?[^\\]?)??'/g, '"$1"'],
     // one liner function declarations to short form
-    [/function (.+?)\s?\n?\s*{\n?\s*(?:return )([^}]+?)\s?;?\s?}/g, (m, p1, p2) => p1.replace(/\s+\(/, '(') + ' = ' + p2],
+    [
+      /function (.+?)\s?\n?\s*{\n?\s*(?:return )([^}]+?)\s?;?\s?}/g,
+      (m, p1, p2) => p1.replace(/\s+\(/, '(') + ' = ' + p2,
+    ],
     // } to end
     [/^(\s*)};?$/gm, '$1end'],
     // { to do
@@ -64,59 +119,71 @@ function convert (text) {
     // strip line end semicolons and spaces
     [/;?\s*$/gm, ''],
     // template literals interpolation fix
-    [/\${(.+?)}/g, '\$($1)'],
+    [/\${(.+?)}/g, '$($1)'],
     // no double new lines
     [/\n\n/g, '\n'],
     [/else if/g, 'elseif'],
     // remove brackets on if statements
     [/if\s*\((.+)\) *\n? *{? */gm, 'if $1 '],
-    [/null/g, 'nothing']
+    [/null/g, 'nothing'],
   ]
-  for (const r of regex)
+  for (const r of regex) {
     text = text.replace(r[0], r[1])
+  }
   return 'using FactCheck\n\n' + text
 }
 
 const polys = {
-  'randstring': {
-    backfill: "randstring(range, len) = join(rand([range...], len))",
-    frontfill: 'using Random'
+  randstring: {
+    backfill: 'randstring(range, len) = join(rand([range...], len))',
+    frontfill: 'using Random',
   },
   '-- default randstring': {
-    backfill: "randstring(len::Int) = randstring(vcat(collect('a':'z'), collect('A':'Z'), collect('0':'9')), len)",
+    backfill:
+      "randstring(len::Int) = randstring(vcat(collect('a':'z'), collect('A':'Z'), collect('0':'9')), len)",
   },
   'rand(s::String)': {
-    backfill: "import Base.rand\n  rand(s::String) = rand([s...])"
+    backfill: 'import Base.rand\n  rand(s::String) = rand([s...])',
   },
-  'replace': {
-    backfill: 'import Base.replace\n  replace(a, p::Pair; count=typemax(Int)) = replace(a, p[1], p[2], count)'
+  replace: {
+    backfill:
+      'import Base.replace\n  replace(a, p::Pair; count=typemax(Int)) = replace(a, p[1], p[2], count)',
   },
-  'occursin': {
-    backfill: 'occursin(re::Regex, s::String) = ismatch\n occursin(c, s::String) = contains(s, string(c))'
+  occursin: {
+    backfill:
+      'occursin(re::Regex, s::String) = ismatch\n occursin(c, s::String) = contains(s, string(c))',
   },
-  'isdefined': {
+  isdefined: {
     description: '@isdefined',
-    backfill: 'macro isdefined(s) isdefined(Symbol(s)) end'
+    backfill: 'macro isdefined(s) isdefined(Symbol(s)) end',
   },
-  'joincharstring': {
+  joincharstring: {
     description: '*(::Char, ::String)',
-    backfill: 'import Base.*\n  *(s::String, c::Char) = s * string(c)\n  *(c::Char, s::String) = string(c) * s'
+    backfill:
+      'import Base.*\n  *(s::String, c::Char) = s * string(c)\n  *(c::Char, s::String) = string(c) * s',
   },
-  'shufflestring': {
+  shufflestring: {
     description: 'shuffle(::String)',
-    endfill: 'shuffle(s::String) = join(shuffle!(split(s, "")), "")'
-  }
+    endfill: 'shuffle(s::String) = join(shuffle!(split(s, "")), "")',
+  },
+  nothing: {
+    description: 'Nothing',
+    backfill: 'Nothing = typeof(nothing)',
+  },
 }
 
 window.onload = () => {
   document.getElementById('text-in').oninput = ev => {
-    document.getElementById('text-out').value = convert(document.getElementById('text-in').value)
+    document.getElementById('text-out').value = convert(
+      document.getElementById('text-in').value
+    )
   }
   const container = document.getElementById('checkboxes')
-  for (let p of Object.keys(polys)) {
+  for (const p of Object.keys(polys)) {
     const label = document.createElement('label')
     const labelText = document.createElement('code')
-    labelText.innerText = 'description' in polys[p] ? polys[p].description : p + '()'
+    labelText.innerText =
+      'description' in polys[p] ? polys[p].description : p + '()'
     const input = document.createElement('input')
     input.setAttribute('name', p)
     input.setAttribute('type', 'checkbox')
@@ -127,9 +194,13 @@ window.onload = () => {
   }
 }
 
-function polyfills () {
-  let backs = new Set(), fronts = new Set(), ends = new Set()
-  for (let box of document.querySelectorAll('#checkboxes [type="checkbox"]')) {
+function polyfills() {
+  const backs = new Set()
+  const fronts = new Set()
+  const ends = new Set()
+  for (const box of document.querySelectorAll(
+    '#checkboxes [type="checkbox"]'
+  )) {
     if (box.checked) {
       const pf = polys[box.name]
       if ('backfill' in pf) {
