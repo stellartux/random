cfa5fec6ce6c94a055db6cf1fe8dcd53b7277277
Danny Robinson <dannyrobinson@gmx.com>
2021-05-19 19:44:14 +0100

    hex keyboard

---
+++ hex-keys/hex-keyboard.js
@@ -0,0 +1,56 @@
+const access = await navigator.requestMIDIAccess({ sysex: true })
+const midiOutputSelect = document.getElementById('midi-outputs')
+
+let midiOutput
+if (access.outputs.length === 1) {
+  midiOutput = access.outputs[0][1]
+}
+window.keymap = null
+
+for (const output of access.outputs) {
+  const option = document.createElement('option')
+  option.value = output[0]
+  option.innerText = output[1].name
+  midiOutputSelect.append(option)
+}
+midiOutputSelect.onchange = () => {
+  midiOutput = access.outputs.get(midiOutputSelect.value)
+}
+
+/**
+ * Loads a JSON file representing a keymap
+ * @param {string} url the URL of the keymap JSON
+ */
+async function loadKeymap(url) {
+  const file = await fetch(url)
+  keymap = await file.json()
+}
+loadKeymap('piano-layout.json')
+
+document.addEventListener('keydown', (event) => {
+  if (!event.repeat && Object.keys(keymap).includes(event.code)) {
+    noteOn(keymap[event.code])
+  }
+})
+
+document.addEventListener('keyup', (event) => {
+  if (Object.keys(keymap).includes(event.code)) {
+    noteOff(keymap[event.code])
+  }
+})
+
+document.getElementById('keymap-file').addEventListener('change', (event) => {
+  const reader = new FileReader()
+  reader.readAsText(event.target.files[0])
+  reader.onload = () => keymap = JSON.parse(reader.result)
+})
+
+function noteOn(pitch, velocity = 100, channel = 0) {
+  console.log(`Noteon: ${pitch}`)
+  midiOutput.send([0x90 | (channel & 0xf), 0x7f & pitch, 0x7f & velocity])
+}
+
+function noteOff(pitch, channel = 0) {
+  console.log(`Noteoff: ${pitch}`)
+  midiOutput.send([0x80 | (channel & 0xf), 0x7f & pitch, 0])
+}
