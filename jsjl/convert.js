d7e4c9795de7202ff5c2ab893c30e4d78c4968cb
Danny Robinson <dannyrobinson@gmx.com>
2021-07-25 06:02:39 +0100

    JS->JL 4 spaces, remove v0.6 stuff

--- jsjl/convert.js
+++ jsjl/convert.js
@@ -27,14 +27,14 @@ function convert(text) {
     // Test.it("description") to context("description") do
     [
       /^\s*(?:[tT]est\.)?it\s*\(\s*["'`](.+?)["'`].*?\n?\s*{/gim,
-      '  context("$1") do',
+      '    context("$1") do',
     ],
     // Test.expect(x, y) to @fact x --> y
     [
       /^\s*[tT]est\.expect\((.+?)\s*,\s*["'`](.+)["'`]\);?}?/gm,
       (match, p1, p2) => {
         return (
-          '    @fact ' +
+          '        @fact ' +
           p1.replace(/(.+)\(/, (m, q1) => q1.toLowerCase().replace('_', '')) +
           ' --> true "' +
           p2 +
@@ -47,7 +47,7 @@ function convert(text) {
       /^\s*(?:[tT]est\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\s*\((.+?\(.*?\))\s*,\s*(.+?),\s*["'](.+?)["']\)+;?/gim,
       (match, p1, p2, p3) => {
         return (
-          '    @fact ' +
+          '        @fact ' +
           p1.replace(
             /(.+)\(/,
             (m, q1) => q1.toLowerCase().replace('_', '') + '('
@@ -62,7 +62,7 @@ function convert(text) {
       /^\s*(?:[tT]est\.)?assert\.?(?:[dD]eep|[sS]trict)?[eE]quals?\((.+?\(.*?\))\s*,\s*(.+?)\);?/gim,
       (match, p1, p2) => {
         return (
-          '    @fact ' +
+          '        @fact ' +
           p1.replace(
             /(.+)\(/g,
             (m, q1) => q1.toLowerCase().replace('_', '') + '('
@@ -75,17 +75,17 @@ function convert(text) {
     // Test.assertSimilar(x, y) to @fact x --> roughly(y)
     [
       /^\s*[tT]est\.assertSimilar\((.+?(?:\(.+?\))?)\s*,\s*(.+?)\);?/gim,
-      '    @fact $1 --> roughly($2)',
+      '        @fact $1 --> $2',
     ],
     // assert.throw(x, y) to @fact_throws y x
     [
       /^\s*(?:[tT]est\.)?assert\.?[tT]hrows?\s*\( *(.+), *(.+) *\)/gm,
-      '    @fact_throws $2 $1',
+      '        @fact_throws $2 $1',
     ],
     // assert.throw(x) to @fact_throws x
     [
       /^\s*(?:[tT]est\.)?assert\.?[tT]hrows?\s*\( *(.+) *\)/gm,
-      '    @fact_throws $1',
+      '        @fact_throws $1',
     ],
     // threequals to twoquals
     [/===/g, '=='],
@@ -133,65 +133,11 @@ function convert(text) {
   return (document.querySelector('input[name="factcheck"]').checked ? 'using FactCheck\n\n' : '') + text
 }
 
-const polys = {
-  randstring: {
-    backfill: 'randstring(range, len) = join(rand([range...], len))',
-    frontfill: 'using Random: randstring',
-  },
-  '-- default randstring': {
-    backfill:
-      "randstring(len::Int) = randstring(['a':'z'..., 'A':'Z'..., '0':'9'...]), len)",
-  },
-  rand: {
-    description: 'rand(s::String)',
-    backfill: 'import Base.rand\n  rand(s::String) = rand([s...])',
-  },
-  replace: {
-    backfill:
-      'import Base.replace\n  function replace(a, ps::Vararg{Pair}; count=typemax(Int))\n    for p in ps\n      a = replace(a, p[1], p[2], count)\n    end\n  a\n  end',
-  },
-  occursin: {
-    backfill:
-      'occursin(re::Regex, s::String) = ismatch(re, s)\n  occursin(c, s::String) = contains(s, string(c))',
-  },
-  isdefined: {
-    description: '@isdefined',
-    backfill: 'macro isdefined(s) isdefined(Symbol(s)) end',
-  },
-  joincharstring: {
-    description: '*(::Char, ::String)',
-    backfill:
-      'import Base.*\n  *(s::String, c::Char) = s * string(c)\n  *(c::Char, s::String) = string(c) * s\n  *(c::Char, s::Char) = string(c) * string(s)',
-  },
-  shufflestring: {
-    description: 'shuffle(::String)',
-    endfill: 'import Random: shuffle\nshuffle(s::String) = [s...] |> shuffle |> join',
-  },
-  nothing: {
-    description: 'Nothing',
-    backfill: 'Nothing = typeof(nothing)',
-  },
-}
-
 window.onload = () => {
   document.getElementById('text-in').oninput = ev => {
     document.getElementById('text-out').value = convert(
       document.getElementById('text-in').value
     )
-  }
-  const container = document.getElementById('checkboxes')
-  for (const p of Object.keys(polys)) {
-    const label = document.createElement('label')
-    const labelText = document.createElement('code')
-    labelText.innerText =
-      'description' in polys[p] ? polys[p].description : p + '()'
-    const input = document.createElement('input')
-    input.setAttribute('name', p)
-    input.setAttribute('type', 'checkbox')
-    input.addEventListener('change', polyfills)
-    label.appendChild(labelText)
-    label.appendChild(input)
-    container.appendChild(label)
   }
 }
 
@@ -208,44 +154,4 @@ function pasteCopy() {
       document.getElementById('text-out').value = code
       navigator.clipboard.writeText(code)
     })
-}
-
-// TODO: remove this
-function polyfills() {
-  const backs = new Set()
-  const fronts = new Set()
-  const ends = new Set()
-  for (const box of document.querySelectorAll(
-    '#checkboxes [type="checkbox"]'
-  )) {
-    if (box.checked) {
-      const pf = polys[box.name]
-      if ('backfill' in pf) {
-        backs.add(pf.backfill)
-      }
-      if ('frontfill' in pf) {
-        fronts.add(pf.frontfill)
-      }
-      if ('endfill' in pf) {
-        ends.add(pf.endfill)
-      }
-    }
-  }
-  let output = ''
-  if (backs.size > 0) {
-    output += ['if VERSION < v"1"', ...backs.values()].join('\n  ')
-  }
-  if (fronts.size > 0) {
-    if (output === '') {
-      output += 'if VERSION >= v"1"'
-    } else {
-      output += '\nelse'
-    }
-    output += '\n  ' + [...fronts.values()].join('\n  ')
-  }
-  if (output !== '') output += '\nend'
-  if (ends.size > 0) {
-    output += ['\n', ...ends.values()].join('\n')
-  }
-  document.getElementById('polyfilloutput').value = output
 }
